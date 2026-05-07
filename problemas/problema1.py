a00249b75995eb89f7d1d5923b469b70201b2c0f
Vianka Castro <viankacastro420@gmail.com>
2026-05-06 18:34:16 -0600

    initial commit y problema 1

---
+++ problemas/problema1.py
@@ -0,0 +1,24 @@
+# PROBLEMA 1. HACER SENCILLO 
+
+# Problema dado un monto m buscamos analizar dicho monto con la 
+# menor cantidad posible de monedas 
+# Las monedas disponibles son: 1, 5, 10, 25
+# Se pueden tomar todas las monedas que necesitemos de cada denominación
+
+
+m = int(input("Ingrese el monto a analizar: "))
+
+lista_monedas = [25, 10, 5, 1]
+resultado = {}
+total_monedas = 0 
+
+for moneda in lista_monedas:
+    cantidad_monedas = m // moneda
+    resultado[moneda] = cantidad_monedas
+    total_monedas += cantidad_monedas
+    m -= cantidad_monedas * moneda
+
+print("Cantidad de monedas necesarias:")
+for moneda, cantidad in resultado.items():
+    print(f"{moneda} centavos: {cantidad}")
+    print(f"Total de monedas utilizadas: {total_monedas}")
